4b28d9b7cfeb1c380fa29048037ed94b826b3909
Rohit Vishwakarma <53476944+Rohit77707@users.noreply.github.com>
2023-01-15 15:57:48 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <div className="card footer">
+        <h2>Git Hub Repo Search | Developed by Rohit &copy; 2023</h2>
+        
+      </div>
+    )
+  }
+  
+  export default Footer;
